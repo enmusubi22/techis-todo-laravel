4d92d334d4a0ea227ca59cabf378dc6f5a47a235
enmusubi22 <ravitora.happy@gmail.com>
2025-01-27 21:47:27 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if(\App::environment(['production'])) {
-            \URL::forseScheme('https');
+        if (\App::environment(['production'])) {
+            \URL::forceScheme('https');
         }
     }
 }
